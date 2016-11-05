1cb0595df66d7ddf24f3567c004582878582d98d
Jacquelyn Torborg <jtorborg1227@comcast.net>
2016-11-05 15:46:13 -0500

    removed animation; added footer image; ng-hide works on tests page

--- public/scripts/tests.controller.js
+++ public/scripts/tests.controller.js
@@ -1,29 +1,35 @@
 angular.module('speechApp')
-.controller('TestsController', TestsController);
+    .controller('TestsController', TestsController);
 
 function TestsController(testsService) {
-  var tests = this;
-  console.log('TestsController loaded');
-  tests.Object = {};
-   tests.Array = [];
-  //get request to /tests
-  this.getTests = function() {
-      console.log('inside getTests');
+    var tests = this;
+    console.log('TestsController loaded');
+    tests.Object = {};
+    tests.Array = [];
+    tests.appropriateTest = true;
 
-      testsService.getTests(tests.age, tests.primarylanguage, tests.concern).then(function(response) {
-        console.log("response", response);;
-console.log(tests.age);
-console.log(tests.primarylanguage);
-console.log(tests.concern);
+    //get request to /tests
+    this.getTests = function() {
+        console.log('inside getTests');
 
-
-        tests.Object = response;
-        console.log('tests object', tests.Object);
-        tests.Array = response.data;
-        console.log("tests array", tests.Array);
-      });
-
-  }
+        testsService.getTests(tests.age, tests.primarylanguage, tests.concern).then(function(response) {
+                console.log("response", response);;
+                console.log(tests.age);
+                console.log(tests.primarylanguage);
+                console.log(tests.concern);
 
 
-}
+                tests.Object = response;
+                console.log('tests object', tests.Object);
+                tests.Array = response.data;
+                console.log("tests array", tests.Array);
+                    if (tests.Array.length === 0) {
+                        tests.appropriateTest = false;
+                    } else {
+                        tests.appropriateTest = true;
+                    }
+                    console.log("tests.appropriateTest", tests.appropriateTest);
+
+                  })
+                };
+    }
