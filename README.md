61168dae3e6680b3eb13d2c94b9a3498083af688
Jacquelyn Torborg <jtorborg1227@comcast.net>
2016-11-07 15:57:21 -0600

    formatting and display changes

--- README.md
+++ README.md
@@ -2,7 +2,16 @@
 
 
 ##Technologies Used
+Postgresql 6.1.0
+Angular 1.5.8
+Express 4.14.0
+Node
+
+Bootstrap 3.3.7
+
+Passport 0.3.2
+
+
 
 ##Author
-
 Jackie Torborg
--- public/scripts/library.controller.js
+++ public/scripts/library.controller.js
@@ -6,49 +6,57 @@ function LibraryController(libraryService) {
     var library = this;
     library.Object = {};
     library.Array = [];
-    // ctrl.library = {
-    //   name: name
-    // }
-    //get request to /library
+    library.Answer = "yes";
 
-    library.getLibrary = function() {
-        console.log('inside getLibrary');
-
-        libraryService.getLibrary().then(function(response) {
-            console.log("response", response);;
-
-            library.Object = response;
-            library.Array = response.data;
-            console.log("library array", library.Array);
-        });
-
-    }
-
-    library.checkoutTest = function(libraryid, libraryavail, librarycomments) {
-        console.log('change availability');
-        console.log("libraryid", libraryid);
-        console.log("librarycomments", librarycomments);
-
-
-        library.testStatus = {
-        libraryid: libraryid,
-        libraryavail: libraryavail,
-        librarycomments: librarycomments
-
-         };
-        console.log('library id', libraryid);
-        console.log('library avail', libraryavail);
-        console.log("librarycomments", librarycomments);
-
-        console.log('library test status', library.testStatus);
-
-
-        libraryService.checkoutTest(library.testStatus).then(function(response) {
-
-          library.getLibrary();
-            console.log("response", response);
-        });
-
-    }
-
+    if (library.available == true) {
+        library.Answer = "yes"
+    } else {
+        library.Answer = "no";
 }
+      console.log('library answer', library.Answer);
+        // ctrl.library = {
+        //   name: name
+        // }
+        //get request to /library
+
+        library.getLibrary = function() {
+            console.log('inside getLibrary');
+
+            libraryService.getLibrary().then(function(response) {
+                console.log("response", response);;
+
+                library.Object = response;
+                library.Array = response.data;
+                console.log("library array", library.Array);
+            });
+
+        }
+
+        library.checkoutTest = function(libraryid, libraryavail, librarycomments) {
+            console.log('change availability');
+            console.log("libraryid", libraryid);
+            console.log("librarycomments", librarycomments);
+
+
+            library.testStatus = {
+                libraryid: libraryid,
+                libraryavail: libraryavail,
+                librarycomments: librarycomments
+
+            };
+            console.log('library id', libraryid);
+            console.log('library avail', libraryavail);
+            console.log("librarycomments", librarycomments);
+
+            console.log('library test status', library.testStatus);
+
+
+            libraryService.checkoutTest(library.testStatus).then(function(response) {
+
+                library.getLibrary();
+                console.log("response", response);
+            });
+
+        }
+
+    }
